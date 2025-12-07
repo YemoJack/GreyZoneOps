e3a91bd7eff7fa3355f253b97410190d91bde680
YemoRed <3391737381@qq.com>
2025-12-07 11:26:23 +0800

    Refactor weapon inventory into data slots

--- Assets/Scripts/Game/Controllers/PlayerController.cs
+++ Assets/Scripts/Game/Controllers/PlayerController.cs
@@ -1,101 +1,20 @@
-﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using QFramework;
-using System;
 
-
-public enum PlayerAnimState
+public class PlayerController : MonoBehaviour, IController
 {
-    Idle,
-    Walk,
-    Run,
-}
-public struct EventPlayerChangeMoveState
-{
-    public EPlayerMoveState PreviousState;
-    public EPlayerMoveState CurrentState;
-}
-
-
-public enum EPlayerMoveState
-{
-    Idle = 0,
-    Walk = 1,
-    Run = 2,
-    Jump = 3,
-    Fall = 4,
-}
-
-
-
-
-
-public class PlayerController : MonoBehaviour,IController
-{
-
-    //public Animator Animator;
-
-    private InputSys inputSys;
-
-    private WeaponSystem weaponSystem;
-
-
     public Transform WeaponRoot;
-
     public List<GameObject> weaponObjectList;
-    private List<WeaponBase> weaponList = new List<WeaponBase>();
 
-    void Start()
+    private PlayerSystem playerSystem;
+
+    private void Start()
     {
         LockCursor(true);
-        inputSys = this.GetSystem<InputSys>();
-        weaponSystem = this.GetSystem<WeaponSystem>();
-
-      
-        this.RegisterEvent<EventPlayerChangeWeapon>(OnPlayerChangeWeapon).UnRegisterWhenGameObjectDestroyed(this);
-
-
-        foreach (var weapon in weaponObjectList)
-        {
-            GameObject wep = Instantiate(weapon,WeaponRoot);
-            wep.transform.localPosition = Vector3.zero;
-            wep.transform.localRotation = Quaternion.identity;
-            weaponList.Add(wep.GetComponent<WeaponBase>());
-        }
-        weaponSystem.EquipWeapon(weaponList);
+        playerSystem = this.GetSystem<PlayerSystem>();
+        playerSystem.InitializeLoadout(WeaponRoot, weaponObjectList);
     }
-
-   
-    private void Update()
-    {
-        if (inputSys.FirePressed)
-        {
-            this.SendCommand<CmdStartAttack>();
-        }
-
-        if(inputSys.Mouse2Pressed)
-        {
-            weaponSystem.SwitchWeapon();
-        }
-
-    }
-
-
-
-    private void OnPlayerChangeWeapon(EventPlayerChangeWeapon weapon)
-    {
-        foreach (var wep in weaponList)
-        {
-            wep.gameObject.SetActive(false);
-            if(wep.InstanceID == weapon.Weapon.InstanceID)
-            {
-                wep.gameObject.SetActive(true);
-            }
-        }
-    }
-
-
 
     public void LockCursor(bool isLocked)
     {
@@ -110,8 +29,6 @@ public class PlayerController : MonoBehaviour,IController
             Cursor.visible = true;
         }
     }
-
-
 
     public IArchitecture GetArchitecture()
     {
--- Assets/Scripts/Game/Systems/PlayerSystem.cs
+++ Assets/Scripts/Game/Systems/PlayerSystem.cs
@@ -1,16 +1,145 @@
-﻿using QFramework;
-using System.Collections;
+using QFramework;
 using System.Collections.Generic;
 using UnityEngine;
 
-public partial class PlayerSystem : AbstractSystem
+public enum PlayerAnimState
 {
+    Idle,
+    Walk,
+    Run,
+}
 
+public struct EventPlayerChangeMoveState
+{
+    public EPlayerMoveState PreviousState;
+    public EPlayerMoveState CurrentState;
+}
 
-    protected override void OnInit() 
+public enum EPlayerMoveState
+{
+    Idle = 0,
+    Walk = 1,
+    Run = 2,
+    Jump = 3,
+    Fall = 4,
+}
+
+public partial class PlayerSystem : AbstractSystem, IUpdateSystem
+{
+    private readonly List<WeaponBase> weaponInstances = new List<WeaponBase>();
+
+    private WeaponSystem weaponSystem;
+    private WeaponInventoryModel weaponInventoryModel;
+    private InputSys inputSys;
+    private SystemUpdateScheduler updateScheduler;
+
+    private EPlayerMoveState moveState = EPlayerMoveState.Idle;
+    private bool initialized;
+
+    protected override void OnInit()
     {
-    
-    
+        weaponSystem = this.GetSystem<WeaponSystem>();
+        weaponInventoryModel = this.GetModel<WeaponInventoryModel>();
+        inputSys = this.GetSystem<InputSys>();
+
+        updateScheduler = this.GetUtility<SystemUpdateScheduler>();
+        updateScheduler.Register(this);
+
+        this.RegisterEvent<EventPlayerChangeWeapon>(OnPlayerChangeWeapon);
     }
 
+    public void InitializeLoadout(Transform weaponRoot, IEnumerable<GameObject> weaponPrefabs)
+    {
+        if (weaponRoot == null || weaponPrefabs == null)
+        {
+            Debug.LogWarning("PlayerSystem InitializeLoadout called with null references.");
+            return;
+        }
+
+        weaponInstances.Clear();
+        foreach (var weaponPrefab in weaponPrefabs)
+        {
+            if (weaponPrefab == null) continue;
+            var weaponObj = Object.Instantiate(weaponPrefab, weaponRoot);
+            weaponObj.transform.localPosition = Vector3.zero;
+            weaponObj.transform.localRotation = Quaternion.identity;
+
+            var weapon = weaponObj.GetComponent<WeaponBase>();
+            if (weapon != null)
+            {
+                weapon.gameObject.SetActive(false);
+                weaponInstances.Add(weapon);
+                weaponSystem.RegisterWeaponInstance(weapon);
+            }
+            else
+            {
+                Debug.LogWarning($"Prefab {weaponPrefab.name} does not contain a WeaponBase component.");
+                Object.Destroy(weaponObj);
+            }
+        }
+
+        weaponSystem.EquipInitialWeapon();
+        initialized = true;
+    }
+
+    public void OnUpdate(float deltaTime)
+    {
+        if (!initialized)
+        {
+            return;
+        }
+
+        HandleWeaponInput();
+        UpdateMoveState();
+    }
+
+    private void HandleWeaponInput()
+    {
+        if (inputSys.FirePressed)
+        {
+            this.SendCommand<CmdStartAttack>();
+        }
+
+        if (inputSys.Mouse2Pressed)
+        {
+            weaponSystem.SwitchWeapon();
+        }
+    }
+
+    private void UpdateMoveState()
+    {
+        var previous = moveState;
+        var moveMagnitude = inputSys.Move2D.magnitude;
+
+        if (moveMagnitude < 0.05f)
+        {
+            moveState = EPlayerMoveState.Idle;
+        }
+        else if (inputSys.Sprint)
+        {
+            moveState = EPlayerMoveState.Run;
+        }
+        else
+        {
+            moveState = EPlayerMoveState.Walk;
+        }
+
+        if (previous != moveState)
+        {
+            this.SendEvent(new EventPlayerChangeMoveState
+            {
+                PreviousState = previous,
+                CurrentState = moveState
+            });
+        }
+    }
+
+    private void OnPlayerChangeWeapon(EventPlayerChangeWeapon evt)
+    {
+        foreach (var weapon in weaponInstances)
+        {
+            if (weapon == null) continue;
+            weapon.gameObject.SetActive(weapon.InstanceID == evt.WeaponInstance?.InstanceID);
+        }
+    }
 }
